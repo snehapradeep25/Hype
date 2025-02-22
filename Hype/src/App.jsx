78b6406f98eb708915af1fc2c7d32c20c643984c
Sneha Pradeep <145252824+snehapradeep25@users.noreply.github.com>
2025-02-22 12:30:16 +0530

    changes made in design

--- Hype/src/App.jsx
+++ Hype/src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Navbar from './components/navbar'
-import { Route, Routes } from 'react-router-dom'
+import { Route,Routes } from "react-router-dom"
 import Home from './pages/home/Home'
 
 const App = () => {
